cd751487392958841f894ad4ea6ea1641d431e34
Adam Pryka <adam.pryka@gmail.com>
2015-10-27 08:42:34 +0100

    new object CustomjQuery

--- custom-jquery.js
+++ custom-jquery.js
@@ -4,35 +4,42 @@ cJQuery=
 {
     console.log("selectot :" + selector + " | " + (typeof selector == 'string'));
     if(typeof selector == 'string')
-        return document.getElementsByClassName(selector);
+        return new CustomjQuery( document.getElementsByClassName(selector));
 
-    return selector;
+    return new CustomjQuery(selector);
 }
 window.$ = cJQuery;
 
-Object.prototype.each = function (fun) {  
-    for (var i = 0; i < this.length; i++) {
-        var obj = this[i];
+
+function CustomjQuery(obj) {
+    this.object = obj;
+}
+
+CustomjQuery.prototype.each = function (fun) {
+    for (var i = 0; i < this.object.length; i++) {
+        var obj = this.object[i];
         fun.call(obj, i);
     }
     return this;
 };
 
 
-Object.prototype.addClass = function (className) {
-    for (i = 0; i < this.length; i++) {
-        this[i].className += (" "+ className);
+
+
+CustomjQuery.prototype.addClass = function (className) {
+    for (i = 0; i < this.object.length; i++) {
+        this.object[i].className += (" " + className);
     }    
     return this;
 };
 
-Object.prototype.html = function (html) {
-    console.log("run: " + this);    
-    if (typeof this.length == 'undefined') {
-        this.innerHTML = html;
+CustomjQuery.prototype.html = function (html) {
+    console.log("html run: " + this.object);
+    if (typeof this.object.length == 'undefined') {
+        this.object.innerHTML = html;
     }    
-    for (i = 0; i < this.length; i++) {
-        this[i].innerHTML = html;
+    for (i = 0; i < this.object.length; i++) {
+        this.object[i].innerHTML = html;
     }
     return this;
 };
